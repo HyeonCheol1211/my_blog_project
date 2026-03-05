f74178204d406e823f7bd941f037a0f3dff3d449
최현철 <guscjf1211@naver.com>
2026-03-05 23:34:25 +0900

    delete frontendFolder

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router' // 위에서 만든 라우터 가져오기
-
-const app = createApp(App)
-
-app.use(router) // 라우터 등록
-app.mount('#app')
